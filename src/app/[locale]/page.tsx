4d6a3b4ca718654ae8ebc350f6cab05de8b8c4d3
Hugo Calmels <calmels.hugo@yahoo.fr>
2025-07-11 08:17:31 +0200

    test

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,10 +11,7 @@ const Home = () => {
         <h1>Bienvenue 👋</h1>
         <p>Je suis Hugo, dev freelance à Toulouse. Sites web, outils personnalisés, code propre.</p>
         <ul>
-          <li><a href="/creation-site-internet-toulouse">Créer un site internet</a></li>
-          <li><a href="/portfolio">Voir mes projets</a></li>
-          <li><a href="/interface-metier">Outils métier</a></li>
-          <li><a href="/contact">Contact</a></li>
+
         </ul>
       </main>
     </>
