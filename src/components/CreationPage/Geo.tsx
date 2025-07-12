7d4f32099a42ec8d6e0a522ee4f36880cfbf3b1d
Hugo Calmels <calmels.hugo@yahoo.fr>
2025-07-12 08:42:22 +0200

    oodn

--- src/components/CreationPage/Geo.tsx
+++ src/components/CreationPage/Geo.tsx
@@ -1,13 +1,12 @@
-'use client'
+'use client';
 
-import styles from './Geo.module.css'
-import { useTranslations } from 'next-intl'
+import styles from './Geo.module.css';
+import { useTranslations } from 'next-intl';
 
 export default function Geo() {
-  const t = useTranslations('geo')
+  const t = useTranslations('geo');
 
-  // Utiliser t.raw pour récupérer le HTML non échappé
-  const descriptionHtml = t.raw('description')
+  const descriptionHtml = t.raw('description');
 
   return (
     <section className={styles.section} id="geo">
@@ -20,6 +19,7 @@ export default function Geo() {
         <div className={styles.mapWrapper}>
           <iframe
             className={styles.map}
+            title="Carte Google Maps – localisation de Hugo Calmels à Toulouse"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46232.32518157955!2d1.403789191906102!3d43.59570671332603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aebc96234d6589%3A0x5b7eaafd50e0210e!2s13%20Rue%20du%20G%C3%A9n%C3%A9ral%20Jean%20P%C3%A9got%2C%2031500%20Toulouse!5e0!3m2!1sfr!2sfr!4v1752039655677!5m2!1sfr!2sfr"
             loading="lazy"
             allowFullScreen
@@ -28,5 +28,5 @@ export default function Geo() {
         </div>
       </div>
     </section>
-  )
+  );
 }
