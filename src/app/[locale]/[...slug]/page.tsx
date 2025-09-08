8b87203eb562d1265383907cd80fe8640f945191
Hugo Calmels <calmels.hugo@yahoo.fr>
2025-09-08 02:40:51 +0200

    done

--- src/app/[locale]/[...slug]/page.tsx
+++ src/app/[locale]/[...slug]/page.tsx
@@ -2,5 +2,6 @@ import { notFound } from 'next/navigation';
 
 export default function CatchAllPage() {
   // 🚨 Cette route n'existe pas statiquement => 404
+  console.log("test");
   return notFound();
 }
